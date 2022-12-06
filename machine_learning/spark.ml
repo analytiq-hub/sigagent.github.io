c9e100530eae35c35be04459299ac6fc51cac6a3
Andrei Radulescu-Banu <44415560+andrei-radulescu-banu@users.noreply.github.com>
2022-12-06 07:14:51 -0500

    *** empty log message ***

---
+++ machine_learning/spark.ml
@@ -0,0 +1,18 @@
+---
+layout: page
+mathjax: true
+title: Spark.ml and Apache Ignite ML
+---
+
+#### Spark.ml resources
+* [spark.ml](https://spark.apache.org/docs/latest/ml-guide.html) official page
+* S. Johnson: [From scikit-learn to Spark ML](https://towardsdatascience.com/from-scikit-learn-to-spark-ml-f2886fb46852) (2019)
+* A. Zinoviev: [Good parts of the Apache Spark ML library](https://zaleslaw.medium.com/i-have-been-a-spark-user-since-spark-0-8-adfb0b327338) (2019)
+* A. Zinoviev: [Weakness of the Apache Spark ML library](https://zaleslaw.medium.com/weakness-of-the-apache-spark-ml-library-41e674103591) (2019)
+
+#### Apache Ignite ML
+* [Apache Ignite ML](https://ignite.apache.org/docs/latest/machine-learning/machine-learning) official page
+* A. Zinoviev: [Apache Ignite ML: origins and development](https://zaleslaw.medium.com/apache-ignite-ml-origins-and-development-d49a19e67202) (2020)
+
+#### Other
+* [Machine Learning](../machine_learning.md)
